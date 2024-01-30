9f5722ed20e9a9695f76c15d1814d485fd6850de
hesam <hesamshayegan88@gmail.com>
2024-01-29 17:48:11 -0800

    implemented the class-based view funtions, activated the debug_toolbar

--- APIs/BookListAPI_II/urls.py
+++ APIs/BookListAPI_II/urls.py
@@ -1,5 +1,8 @@
-from django.urls import path 
+from django.urls import path, include 
 from . import views
 urlpatterns = [
-    path('books/', views.books)
+    # path('books/', views.books)
+    path('books', views.BookList.as_view()),
+    path('books/<int:pk>', views.Book.as_view()),
+    path('__debug__/', include('debug_toolbar.urls'))
 ]
--- APIs/BookListAPI_II/views.py
+++ APIs/BookListAPI_II/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 
 # @api_view()
 # def books(request):
@@ -10,3 +11,22 @@ from rest_framework.decorators import api_view
 @api_view(['GET', 'POST'])
 def books(request):
     return Response('list of the books', status=status.HTTP_200_OK)
+
+
+# class based views
+class BookList(APIView):
+    def get(self, request):
+        author = request.GET.get('author')
+        if(author):
+            return Response({"message": "list of the books by " + author}, status.HTTP_200_OK)
+        return Response({"message": "list of the books"}, status.HTTP_200_OK)
+    
+    def post(self, request):
+        return Response({"title": request.data.get('title')}, status.HTTP_201_CREATED)
+    
+class Book(APIView):
+    def get(self, request, pk):
+        return Response({"message": "single book with id " + str(pk)}, status.HTTP_200_OK)
+    
+    def put(self, request, pk):
+        return Response({"title": request.data.get('title')}, status.HTTP_200_OK)
